c05253d58617ceb93eb6a8c149db595fe6214544
yanwu20 <xiaoyu.wang1@outlook.com>
2021-07-08 16:04:44 +1000

    add comments

--- prediction.py
+++ prediction.py
@@ -20,10 +20,11 @@ def prediction():
     output = dict()
     X_CNN = pd.read_csv("./features/CNN_features.csv", index_col=0)
     X_XGB = pd.read_csv("./features/XGB_features.csv", header=None).iloc[587:, 1:]
+    label = X_CNN.index
     CNN_model, XGB_model = loadmodel()
     output["CNN"] = CNN_model.predict(X_CNN).T[0]
     output["XGB"] = XGB_model.predict_proba(X_XGB)[:, 1]
-    df_level1 = pd.DataFrame(output)
+    df_level1 = pd.DataFrame(output,index=label)
     df_level1["mean"] = 0.5 * df_level1["CNN"] + 0.5 * df_level1["XGB"]
     df_level1["pred"] = np.rint(df_level1["mean"])
     return df_level1
