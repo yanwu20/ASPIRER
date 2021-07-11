88a1c4a9802dd889ef7fdc6615531e28ff5e6a10
yanwu20 <xiaoyu.wang1@outlook.com>
2021-07-11 11:34:16 +1000

    integrate to main file

--- featureGenerator.py
+++ featureGenerator.py
@@ -1,3 +1,5 @@
+import sys
+
 import pandas as pd
 import numpy as np
 import readFasta
@@ -27,13 +29,14 @@ def CNN_inputGenterator(fasta, length=60):
     return index, encoding
 
 
-def XGB_inputGenterator(fasta,**kw):
-    features = ["AAC", "CKSAAP", "Pse_pssm", "CTDC", "CTDT", "CTDD", "CTriad", "DDE", "GAAC", "GDPC", "Moran", "DPC", "TPC"]
+def XGB_inputGenterator(fasta, **kw):
+    features = ["AAC", "CKSAAP", "Pse_pssm", "CTDC", "CTDT", "CTDD", "CTriad", "DDE", "GAAC", "GDPC", "Moran", "DPC",
+                "TPC"]
     feature_dict = {}
     feature_list = []
     for i in features:
-        if i =="Pse_pssm" and kw["PSSM"]:
-            feature_list.append(pd.read_csv(kw["PSSM"],header=None))
+        if i == "Pse_pssm" and kw["PSSM"]:
+            feature_list.append(pd.read_csv(kw["PSSM"]))
         else:
             cmd = i + '.' + i + '(fasta, **kw)'
             encoding = eval(cmd)
@@ -44,14 +47,29 @@ def XGB_inputGenterator(fasta,**kw):
     print(df)
     return df
 
-PSSM_file = "./features/pse_pssm_test.csv"
-kw = {'order': 'ACDEFGHIKLMNPQRSTVWY','PSSM': PSSM_file}
-fasta_N = readFasta.readfasta("./dataset/PeNGaRoo_independent_test_N.fasta")
-fasta_P = readFasta.readfasta("./dataset/PeNGaRoo_independent_test_P.fasta")
-fasta = fasta_N + fasta_P
-CNN_index, CNN_input = CNN_inputGenterator(fasta)
-df_cnn = pd.DataFrame(CNN_input, index=CNN_index)
-# df_cnn.to_csv("./features/CNN_features.csv")
-df_XGB = XGB_inputGenterator(fasta, **kw)
-# df_XGB.to_csv("./features/XGB_features.csv")
 
+def generator(PSSM_file=None, fasta_file=None, output_name=""):
+    if not PSSM_file and not fasta_file:
+        PSSM_file = "./features/pse_pssm_test.csv"
+        fasta_N = readFasta.readfasta("./dataset/PeNGaRoo_independent_test_N.fasta")
+        fasta_P = readFasta.readfasta("./dataset/PeNGaRoo_independent_test_P.fasta")
+        fasta = fasta_N + fasta_P
+    elif PSSM_file and fasta_file:
+        fasta = readFasta.readfasta(fasta_file)
+    else:
+        print("Missing input files")
+        sys.exit(1)
+    kw = {'order': 'ACDEFGHIKLMNPQRSTVWY', 'PSSM': PSSM_file}
+    CNN_index, CNN_input = CNN_inputGenterator(fasta)
+    df_cnn = pd.DataFrame(CNN_input, index=CNN_index)
+    df_XGB = XGB_inputGenterator(fasta, **kw)
+    if not output_name:
+        df_cnn.to_csv("./features/CNN_features.csv")
+        df_XGB.to_csv("./features/XGB_features.csv", index=False, header=False)
+    else:
+        df_cnn.to_csv("./features/CNN_features_%s.csv" % output_name)
+        df_XGB.to_csv("./features/XGB_features_%s.csv" % output_name, index=False, header=False)
+
+
+if __name__ == "__main__":
+    generator()
--- prediction.py
+++ prediction.py
@@ -3,9 +3,11 @@ import random
 import joblib
 import numpy as np
 import pandas as pd
+from sklearn.metrics import confusion_matrix, roc_auc_score, average_precision_score
 from tensorflow.python.keras.models import load_model
 
 random.seed(10)
+
 
 # load model from the model file
 def loadmodel():
@@ -15,20 +17,44 @@ def loadmodel():
     XGB_model = joblib.load(XGB_model_path)
     return CNN_model, XGB_model
 
+def valiadation(y_test,y_pred,y_score):
+    con_matrix = confusion_matrix(y_test,y_pred,labels=[0,1])
+    tn, fp, fn, tp = con_matrix.ravel()
+    acc = (tp+tn)/(tp+fp+tn+fn)
+    precision = tp/(tp+fp)
+    recall = tp/(tp+fn)
+    specificity = tn/(tn+fp)
+    f1_score = 2*(recall*precision)/(recall+precision)
+    mcc = (tp*tn-fp*fn)/math.sqrt((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn))
+    AUROC = roc_auc_score(y_test,y_score)
+    AUPRC = average_precision_score(y_test, y_score)
+    performance = [recall,specificity,precision,acc,mcc,f1_score,AUROC,AUPRC]
+    return performance
+
 # make the prediction based on CNN model and XGBoost model
-def prediction():
+def prediction(output_name=""):
+    if output_name:
+        X_XGB = pd.read_csv("./features/XGB_features_%s.csv" % output_name, header=None)
+        X_CNN = pd.read_csv("./features/CNN_features_%s.csv" % output_name, index_col=0)
+        output_file = "./output/%s.csv" % output_name
+    else:
+        X_XGB = pd.read_csv("./features/XGB_features.csv", header=None)
+        X_CNN = pd.read_csv("./features/CNN_features.csv", index_col=0)
+        output_file = "./output/ASPIERE_result.csv"
     output = dict()
-    X_CNN = pd.read_csv("./features/CNN_features.csv", index_col=0)
-    X_XGB = pd.read_csv("./features/XGB_features.csv", header=None).iloc[587:, 1:]
     label = X_CNN.index
     CNN_model, XGB_model = loadmodel()
     output["CNN"] = CNN_model.predict(X_CNN).T[0]
     output["XGB"] = XGB_model.predict_proba(X_XGB)[:, 1]
-    df_level1 = pd.DataFrame(output,index=label)
+    df_level1 = pd.DataFrame(output, index=label)
     df_level1["mean"] = 0.5 * df_level1["CNN"] + 0.5 * df_level1["XGB"]
     df_level1["pred"] = np.rint(df_level1["mean"])
+    df_level1.to_csv(output_file)
+    y_test = np.append(np.zeros(34),np.ones(34))
+    # print(valiadation(y_test,df_level1["pred"],df_level1["mean"]))
+    print(df_level1)
     return df_level1
 
 
 if __name__ == "__main__":
-    print(prediction())
+    prediction()
